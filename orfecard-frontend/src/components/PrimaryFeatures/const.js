8f4d8a3cd7fdf6e50646788a8f7cb4cda156f235
alikonuk12 <alikonuk12@gmail.com>
2022-06-12 01:11:03 +0300

    Primary features is added

---
+++ orfecard-frontend/src/components/PrimaryFeatures/const.js
@@ -0,0 +1,27 @@
+export const ELEMENT = [
+    {
+        title: 'Güvenlik',
+        text: 'Yalnızca izin verdiğiniz bilgiler paylaşılır',
+        url: 'images/primary_features_images/image_1.png'
+    },
+    {
+        title: 'Çevre Dostu',
+        text: 'Kağıt kullanımını sıfıra indirir. Çevreci ve sürdürülebilir',
+        url: 'images/primary_features_images/image_2.png'
+    },
+    {
+        title: 'Hızlı Paylaşım',
+        text: 'Saniyede istediğiniz tüm bilgileri görüntülenir',
+        url: 'images/primary_features_images/image_3.png'
+    },
+    {
+        title: 'Temassız İşlem',
+        text: 'Temassız teknoloji sayesinde hijyenik işlem sağlar',
+        url: 'images/primary_features_images/image_4.png'
+    },
+    {
+        title: 'Para İade Garantisi',
+        text: 'Memnun kalmadığınız takdirde para iade garantisi',
+        url: 'images/primary_features_images/image_5.png'
+    }
+];
--- orfecard-frontend/src/components/PrimaryFeatures/index.js
+++ orfecard-frontend/src/components/PrimaryFeatures/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './index.module.scss';
+import { ELEMENT } from './const';
 
 const PrimaryFeatures = () => {
     return (
         <div className={styles.container}>
-
+            {ELEMENT.map(({ title, text, url }) =>
+                <div className={styles.elementContainer} key={title}>
+                    <img
+                        className={styles.image}
+                        src={url}
+                        alt='primary_features'
+                    />
+                    <div className={styles.title}>{title}</div>
+                    <div className={styles.text}>{text}</div>
+                </div>
+            )}
         </div>
     );
 }
