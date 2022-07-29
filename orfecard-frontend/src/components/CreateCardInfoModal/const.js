a9f7ad914e5ebb9f879e5b474f6c1c0d1f8cafc9
alikonuk12 <alikonuk12@gmail.com>
2022-07-29 20:16:10 +0300

    add CreateCardInfoModal component

---
+++ orfecard-frontend/src/components/CreateCardInfoModal/const.js
@@ -0,0 +1,46 @@
+export const FIELDS = {
+    name: 'Ad:',
+    lastname: 'Soyad:',
+    phoneNumber: 'Telefon Numarası:',
+    landlineNumber: 'Sabit Hat:',
+    extNumber: 'Dahili No:',
+    email: 'E-posta:',
+    website: 'Websitesi:',
+    address: 'Adres:',
+    location: 'Konum:',
+    companyName: 'Şirket:'
+}
+
+export const SOCIAL_MEDIA_FIELDS = {
+    facebook: 'facebook.svg',
+    twitter: 'twitter.svg',
+    instagram: 'instagram.svg',
+    linkedIn: 'linkedin.svg',
+    youtube: 'youtube.svg',
+    pinterest: 'pinterest.svg'
+}
+
+export const E_COMMERCE_FIELDS = {
+    sahibinden: 'sahibinden.svg',
+    shopier: 'shopier.svg',
+    yemeksepeti: 'yemeksepeti.svg',
+    trendyol: 'trendyol.svg',
+    trendyolYemek: 'trendyolyemek.svg',
+    amazon: 'amazon.svg',
+    n11: 'n11.svg',
+    cicekSepeti: 'ciceksepeti.svg',
+    letgo: 'letgo.svg',
+    hepsiburada: 'hepsiburada.svg',
+    grupanya: 'grupanya.svg',
+    getir: 'getir.svg',
+    etsTur: 'etstur.svg'
+}
+
+export const VALIDATION_TEXT = {
+    REQUIRED: 'Doldurulması gereklidir',
+    LENGTH: 'Yeterli uzunlukta değildir',
+    EMAIL: 'E-mail formatında olmalıdır',
+    PHONE_NUMBER: 'Numaranız 05 ile başlamalıdır'
+}
+
+export const BUTTON_TEXT = 'Ekle';
