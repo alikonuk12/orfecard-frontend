0ba40065f190cc5292b8020dbc272e1f14288b92
alikonuk12 <alikonuk12@gmail.com>
2022-09-27 00:14:37 +0300

    add TeslimatVeIadeKosullari page

---
+++ orfecard-frontend/src/pages/TeslimatVeIadeKosullari/const.js
@@ -0,0 +1,70 @@
+export const HEADER = 'TESLİMAT & İADE KOŞULLARI';
+export const FIRST = [
+    'GENEL',
+    'Kullanmakta olduğunuz web sitesi üzerinden elektronik ortamda sipariş verdiğiniz takdirde, size sunulan ön bilgilendirme formunu ve mesafeli satış sözleşmesini kabul etmiş sayılırsınız.',
+    'Alıcılar, satın aldıkları ürünün satış ve teslimi ile ilgili olarak 6502 sayılı Tüketicinin Korunması Hakkında Kanun ve Mesafeli Sözleşmeler Yönetmeliği (RG:27.11.2014/29188) hükümleri ile yürürlükteki diğer yasalara tabidir.',
+    'Ürün sevkiyat masrafı olan kargo ücretleri alıcılar tarafından ödenecektir.',
+    'Satın alınan her bir ürün, 30 günlük yasal süreyi aşmamak kaydı ile alıcının gösterdiği adresteki kişi ve/veya kuruluşa teslim edilir. Bu süre içinde ürün teslim edilmez ise, Alıcılar sözleşmeyi sona erdirebilir.',
+    'Satın alınan ürün, eksiksiz ve siparişte belirtilen niteliklere uygun ve varsa garanti belgesi, kullanım kılavuzu gibi belgelerle teslim edilmek zorundadır.',
+    'Satın alınan ürünün satılmasının imkansızlaşması durumunda, satıcı bu durumu öğrendiğinden itibaren 3 gün içinde yazılı olarak alıcıya bu durumu bildirmek zorundadır. 14 gün içinde de toplam bedel Alıcı’ya iade edilmek zorundadır.',
+    'Ön sipariş ürününde(lerinde) , işbu 30 günlük yasal süre ilgili ürüne ait satış sayfasında ilan edilen satış tarihinden itibaren başlayacak olup, işbu satış tarihinde tedarikçi firma kaynaklı gecikmeler yaşanabilir. Ön sipariş sisteminde, planlanan teslimat süresi, talep toplama, üretim planlama ve sevk süresine bağlı olarak en fazla 10 haftaya kadar uzayabilecektir. Mücbir sebep hallerinde (öngörülemeyen ve önüne geçilemeyen durumlar) teslim süresi işlemeyecektir. İşbu halde SATICI ALICI’yı öncesinde yazılı olarak bilgilendirecektir. Bu halde de ALICI siparişin iptal edilmesini veya teslimat süresini bekleyerek siparişe konu ürün(lerin) teslim edilmesi haklarından birini kullanabilir. ALICI’nın siparişi iptal etmesi halinde, ön sipariş sistemi kullanılarak satın alınan ürünler ile ilgili iade / iptal talepleri, talebin yazılı olarak iletilmesini takip eden 60 gün içinde ürün bedeli alıcıya iade edilir.',
+    'Taksitle satın alınan ürünlere ait yapılan iptaller, ödeme yapılan kart hesabına tekrar taksitli olarak iade edilmektedir.'
+];
+export const SECOND = [
+    'SATIN ALINAN ÜRÜN BEDELİ ÖDENMEZ İSE:',
+    'Alıcı, satın aldığı ürün bedelini ödemez veya banka kayıtlarında iptal ederse, Satıcının ürünü teslim yükümlülüğü sona erer.'
+];
+export const THIRD = [
+  'KREDİ KARTININ YETKİSİZ KULLANIMI İLE YAPILAN ALIŞVERİŞLER:',
+  'Ürün teslim edildikten sonra, alıcının ödeme yaptığı kredi kartının yetkisiz kişiler tarafından haksız olarak kullanıldığı tespit edilirse ve satılan ürün bedeli ilgili banka veya finans kuruluşu tarafından Satıcı’ya ödenmez ise, Alıcı, sözleşme konusu ürünü 3 gün içerisinde nakliye gideri SATICI’ya ait olacak şekilde SATICI’ya iade etmek zorundadır.',
+];
+export const FOURTH = [
+    'ÖNGÖRÜLEMEYEN SEBEPLERLE ÜRÜN SÜRESİNDE TESLİM EDİLEMEZ İSE:',
+    'Satıcının öngöremeyeceği mücbir sebepler oluşursa ve ürün süresinde teslim edilemez ise, durum Alıcı’ya bildirilir. Alıcı, siparişin iptalini, ürünün benzeri ile değiştirilmesini veya engel ortadan kalkana dek teslimatın ertelenmesini talep edebilir. Alıcı siparişi iptal ederse; ödemeyi nakit ile yapmış ise iptalinden itibaren 14 gün içinde kendisine nakden bu ücret ödenir. Alıcı, ödemeyi kredi kartı ile yapmış ise ve iptal ederse, bu iptalden itibaren yine 14 gün içinde ürün bedeli bankaya iade edilir, ancak bankanın alıcının hesabına 2-3 hafta içerisinde aktarması olasıdır.',
+];
+export const FIFTH = [
+    'ALICININ ÜRÜNÜ KONTROL ETME YÜKÜMLÜLÜĞÜ:',
+    'Alıcı, sözleşme konusu mal/hizmeti teslim almadan önce muayene edecek; ezik, kırık, ambalajı yırtılmış vb. hasarlı ve ayıplı mal/hizmeti kargo şirketinden teslim almayacaktır. Teslim alınan mal/hizmetin hasarsız ve sağlam olduğu kabul edilecektir. ALICI , Teslimden sonra mal/hizmeti özenle korunmak zorundadır. Cayma hakkı kullanılacaksa mal/hizmet kullanılmamalıdır. Ürünle birlikte Fatura da iade edilmelidir.'
+];
+export const SIXTH = [
+  'CAYMA HAKKI:',
+  'ALICI; ‘’cayma hakkı kullanılamayacak ürünler’’ başlığında belirtilen istisnalar hariç olmak üzere satın aldığı ürünün kendisine veya gösterdiği adresteki kişi/kuruluşa teslim tarihinden itibaren 7 (yedi) gün içerisinde, SATICI’ya aşağıdaki iletişim bilgileri üzerinden bildirmek şartıyla hiçbir hukuki ve cezai sorumluluk üstlenmeksizin ve hiçbir gerekçe göstermeksizin malı reddederek sözleşmeden cayma hakkını kullanabilir.',
+  'SATICININ CAYMA HAKKI BİLDİRİMİ YAPILACAK İLETİŞİM BİLGİLERİ:',
+  'ŞİRKET',
+  'Ünvanı : Ozan ASLAN',
+  'Eticaret site adresi: https://www.orfecard.com/',
+  'Adresi : Osmanağa Mah. Bestekar Dilhayat Sk. No:3/32 Kadıköy/İST.',
+  'Telefon : +90 216 700 2915',
+  'Faks : -',
+  'E-posta : support@orfecard.com'
+];
+export const SEVENTH = [
+  'CAYMA HAKKININ SÜRESİ:',
+  'Cayma hakkının kullanılması için 7 (yedi) günlük süre içinde SATICI’ ya iadeli taahhütlü posta, faks veya eposta ile yazılı bildirimde bulunulması ve ürünün işbu sözleşmede düzenlenen “Cayma Hakkı Kullanılamayacak Ürünler” hükümleri çerçevesinde kullanılmamış olması şarttır.',
+];
+export const EIGHTH = [
+  'CAYMA HAKKININ KULLANIMI:',
+  '3. kişiye veya ALICI’ ya teslim edilen ürünün faturası, (İade edilmek istenen ürünün faturası kurumsal ise, iade ederken kurumun düzenlemiş olduğu iade faturası ile birlikte gönderilmesi gerekmektedir. Faturası kurumlar adına düzenlenen sipariş iadeleri İADE FATURASI kesilmediği takdirde tamamlanamayacaktır.) iade edilecek ürünlerin kutusu, ambalajı, varsa standart aksesuarları ile birlikte eksiksiz ve hasarsız olarak teslim edilmesi gerekmektedir.'
+];
+export const NINTH = [
+  'İADE KOŞULLARI:',
+  'SATICI, cayma bildiriminin kendisine ulaşmasından itibaren en geç 14 günlük süre içerisinde toplam bedeli ve ALICI’yı borç altına sokan belgeleri ALICI’ ya iade etmek ve 20 günlük süre içerisinde malı iade almakla yükümlüdür.',
+  'Ön sipariş sistemi kullanılarak satın alınan ürünler ile ilgili iade / iptal talepleri, talebin yazılı olarak iletilmesini takip eden 60 gün içinde ürün bedeli alıcıya iade edilir. Taksitle satın alınan ürünlere ait yapılan iptaller, ödeme yapılan kart hesabına tekrar taksitli olarak iade edilmektedir.',
+  'Cayma hakkının kullanılması nedeniyle SATICI tarafından düzenlenen kampanya limit tutarının altına düşülmesi halinde kampanya kapsamında faydalanılan indirim miktarı iptal edilir.'
+];
+export const TENTH = [
+  'CAYMA HAKKI KULLANILAMAYACAK ÜRÜNLER:',
+  'Niteliği itibarıyla iade edilemeyecek ürünler, tek kullanımlık ürünler, kopyalanabilir yazılım ve programlar, hızlı bozulan veya son kullanım tarihi geçen ürünler için cayma hakkı kullanılamaz. Aşağıdaki ürünlerde cayma hakkının kullanılması, ürünün ambalajının açılmamış, bozulmamış ve ürünün kullanılmamış olması şartına bağlıdır.',
+  '-Her türlü NFC ürünler'
+];
+export const ELEVENTH = [
+  'TEMERRÜT HALİ VE HUKUKİ SONUÇLARI',
+  'ALICI, ödeme işlemlerini kredi kartı ile yaptığı durumda temerrüde düştüğü takdirde, kart sahibi banka ile arasındaki kredi kartı sözleşmesi çerçevesinde faiz ödeyeceğini ve bankaya karşı sorumlu olacağını kabul, beyan ve taahhüt eder. Bu durumda ilgili banka hukuki yollara başvurabilir; doğacak masrafları ve vekâlet ücretini ALICI’dan talep edebilir ve her koşulda ALICI’nın borcundan dolayı temerrüde düşmesi halinde, ALICI, borcun gecikmeli ifasından dolayı SATICI’nın uğradığı zarar ve ziyanını ödeyeceğini kabul eder.'
+];
+
+export const TWELFTH = [
+  'ÖDEME VE TESLİMAT',
+  'Online ödemelerinizde siparişiniz sonunda kredi kartınızdan tutar çekim işlemi gerçekleşecektir.',
+  'Talepleriniz sipariş sonunda teslimata hazırlanmak üzere işleme alınır. Siparişiniz onaylandıktan sonra en geç 5 (beş) iş günü sonunda kargo firmasına teslim edilir. Stokta bulunmayan ürünler için teslimat süresi 6 – 8 hafta olacaktır. Kargonuzun durumu siparişinizin sonucunda size verilecek kargo takip numarası ile izlenebilir. Sözleşme konusunu oluşturan ürünler SATICI’nın belirleyeceği kargo firması tarafından ALICI’ya teslim edilir. Kargo ücreti ve varsa kargo sigorta ücreti fatura bedeline eklenir. ALICI bu bedelleri ödemeyi de peşinen kabul ve taahhüt eder. Ödeme işlemi yapıldıktan sonra sözleşme konusu ürünlerin yurtiçi kargoların teslim etme süresi ortalama 1 gün ila 2 hafta sürmektedir.',
+  'Ön sipariş sistemi yapılan işlemlerde, teslimat ürüne ait satış sayfasında ilan edilen satış tarihinden itibaren başlayacak olup, işbu satış tarihinde tedarikçi firma kaynaklı gecikmeler yaşanabilir. İşbu halde SATICI ALICI’yı öncesinde yazılı olarak bilgilendirecektir.'
+];
---
+++ orfecard-frontend/src/pages/TeslimatVeIadeKosullari/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { HEADER, FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, NINTH, TENTH, ELEVENTH, TWELFTH } from './const';
+import styles from './index.module.scss';
+
+const TeslimatVeIadeKosullari = () => {
+    const { mode } = useSelector(state => state.view);
+
+    return (
+        <div className={mode === 'DESKTOP' ? styles.container : styles.mobileContainer}>
+            <div className={styles.header}>{HEADER}</div>
+            <div className={styles.content}>{FIRST}</div>
+            <div className={styles.content}>{SECOND.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{THIRD.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{FOURTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{FIFTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{SIXTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{SEVENTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{EIGHTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{NINTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{TENTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{ELEVENTH.map(el => <div>{el}</div>)}</div>
+            <div className={styles.content}>{TWELFTH.map(el => <div>{el}</div>)}</div>
+        </div>
+    );
+}
+
+export default TeslimatVeIadeKosullari;
