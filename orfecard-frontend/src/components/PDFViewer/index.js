bac308a31c04c59f9e9856005a5ce0df65d9ab1a
alikonuk12 <alikonuk12@gmail.com>
2022-09-27 21:28:57 +0300

    fix PDFViewer bug

--- orfecard-frontend/src/components/PDFViewer/index.js
+++ orfecard-frontend/src/components/PDFViewer/index.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Document, pdfjs, Page } from "react-pdf";
 import "./styles.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFViewer = ({ pdf }) => {
+    const [numPages, setNumPages] = useState(0);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    }
     return (
-        <Document file={pdf}>
-            <Page pageNumber={1} />
+        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+            {Array.from(new Array(numPages), (_, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
         </Document>
     );
 }
