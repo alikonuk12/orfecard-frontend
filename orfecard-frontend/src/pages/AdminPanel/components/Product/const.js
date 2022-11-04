f1eafdd73e13972c9140fb0fd01e558e03f6c96f
alikonuk12 <alikonuk12@gmail.com>
2022-11-04 17:09:54 +0300

    add navigate to specific ID in admin panel

--- orfecard-frontend/src/pages/AdminPanel/components/Product/const.js
+++ orfecard-frontend/src/pages/AdminPanel/components/Product/const.js
@@ -2,7 +2,16 @@ export const COLUMNS = [
     {
         Header: () => <div style={{ textAlign: 'start', marginBottom: 10 }}>ID</div>,
         accessor: '_id',
-        Cell: ({ value }) => <div style={{ width: 225, wordBreak: "break-all" }}>{value}</div>
+        Cell: ({ value }) =>
+            <a
+                id={value}
+                href={`/adminpanel/dashboard/product/${value}`}
+                style={{ display: 'block', width: 225, wordBreak: "break-all", textDecoration: 'none', color: 'blueviolet' }}
+                onMouseOver={() => document.getElementById(value).style.textDecoration = 'underline'}
+                onMouseLeave={() => document.getElementById(value).style.textDecoration = 'none'}
+            >
+                {value}
+            </a>
     },
     {
         Header: () => <div style={{ textAlign: 'start', marginBottom: 10 }}>Ürün Adı</div>,
